416680d390dc6905ad3bd35296b0bc23391e0602
xuhexin <xuhxin@yonyou.com>
2023-08-03 15:32:06 +0800

    feat: 实现JSX

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,59 @@
+import { REACT_ELEMENT_TYPE } from '../../share/ReactSymbol';
+import {
+	Type,
+	Ref,
+	Key,
+	Props,
+	ElementType,
+	ReactElement
+} from '../../share/ReactType';
+const ReactElement = function (
+	type: Type,
+	ref: Ref,
+	key: Key,
+	props: Props
+): ReactElement {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		ref,
+		key,
+		props,
+		__mark: 'Xin'
+	};
+
+	return element;
+};
+
+export const jsx = (
+	type: ElementType,
+	config: any,
+	...maybeChldren: any
+): ReactElement => {
+	let key: Key = null;
+	let ref: Ref = null;
+	const props: Props = null;
+	for (const propName in config) {
+		if (propName === 'key' && config[propName] !== undefined) {
+			key = config[propName];
+			continue;
+		}
+		if (propName === 'ref' && config[propName] !== undefined) {
+			ref = config[propName];
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, propName)) {
+			props[propName] = config[propName];
+		}
+	}
+	if (maybeChldren.length > 0) {
+		if (maybeChldren.length === 1) {
+			props.children = maybeChldren[0];
+		} else {
+			props.children = maybeChldren;
+		}
+	}
+	return ReactElement(type, ref, key, props);
+};
+
+export const jsxDEV = jsx;
